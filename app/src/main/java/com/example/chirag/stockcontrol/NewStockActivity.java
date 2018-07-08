be47478309b568236f848e424759fcbe6e37d496
cvbutani <chirag.butani1@gmail.com>
2018-07-08 18:02:48 -0400

    Date Picker

--- app/src/main/java/com/example/chirag/stockcontrol/NewStockActivity.java
+++ app/src/main/java/com/example/chirag/stockcontrol/NewStockActivity.java
@@ -1,18 +1,57 @@
 package com.example.chirag.stockcontrol;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class NewStockActivity extends AppCompatActivity {
+
+    private TextView tvDatePicker;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_item);
+
+        tvDatePicker = findViewById(R.id.edit_item_date);
+        tvDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        NewStockActivity.this,
+                        android.R.style.Theme_Holo_Dialog_NoActionBar,
+                        mDateSetListener,
+                        year, month, day);
+
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                String date = dayOfMonth + "/" + month + "/" + year ;
+                tvDatePicker.setText(date);
+            }
+        };
     }
 
     @Override
